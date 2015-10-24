ab6ec62a607e34d6b5527a7484970fb47e73c7c3
wolf00 <ladrahul00@gmail.com>
2015-10-24 18:19:50 +0530

    Page1

---
+++ hello/www/CustomerData.js
@@ -0,0 +1,82 @@
+[
+	{
+		"Name" : "Rahul",
+		"Address" : {
+			"Latitude" : 18.12,
+			"Longitude" : 21.26,
+			"House_No" : 221,
+			"Appartment_Name" : "Baker's Street",
+			"Street" : "Baker's Street",
+			"City" : "London",
+			"State" : "Bermingham",
+			"Landmark" : "S. H.",
+			"PIN" : 411043
+		},
+		"Email" : "ladrahul00@gmail.com",
+		"Password" : "1234",
+		"Mobile" : 8983102771,
+		"Saved_Addresses" : [],
+		"Current_Orders" : [],
+		"Previous_Orders" : []
+	},
+	{
+		"Name" : "Jayti",
+		"Address" : {
+			"Latitude" : 12.12,
+			"Longitude" : 24.26,
+			"House_No" : 222,
+			"Appartment_Name" : "Vidya Niketan",
+			"Street" : "Baker's Street",
+			"City" : "China",
+			"State" : "Bermingham",
+			"Landmark" : "Near Chau-Minal",
+			"PIN" : 411043
+		},
+		"Email" : "jaytilal@gmail.com",
+		"Password" : "1234",
+		"Mobile" : 9998956230,
+		"Saved_Addresses" : [],
+		"Current_Orders" : [],
+		"Previous_Orders" : []
+	},
+	{
+		"Name" : "Minal",
+		"Address" : {
+			"Latitude" : 14.12,
+			"Longitude" : 61.26,
+			"House_No" : 12,
+			"Appartment_Name" : "Gangadham",
+			"Street" : "Gangadham kondwa Street",
+			"City" : "Pune",
+			"State" : "Maharashtra",
+			"Landmark" : "Bhaiya",
+			"PIN" : 411047
+		},
+		"Email" : "minal.laad@gmail.com",
+		"Password" : "1234",
+		"Mobile" : 7507167384,
+		"Saved_Addresses" : [],
+		"Current_Orders" : [],
+		"Previous_Orders" : []
+	},
+	{
+		"Name" : "Karan",
+		"Address" : {
+			"Latitude" : 58.12,
+			"Longitude" : 20.26,
+			"House_No" : 22,
+			"Appartment_Name" : "Shraddhaanusar",
+			"Street" : "Katraj tunnel",
+			"City" : "Tokyo",
+			"State" : "Japan",
+			"Landmark" : "Rakuten",
+			"PIN" : 011043
+		},
+		"Email" : "thanvikaran95@gmail.com",
+		"Password" : "1234",
+		"Mobile" : 8983456212,
+		"Saved_Addresses" : [],
+		"Current_Orders" : [],
+		"Previous_Orders" : []
+	}
+]
---
+++ hello/www/ShopkeeperData.js
@@ -0,0 +1,102 @@
+[
+	{
+		"Shopname" : "Motu pheku shop",
+		"Address" : {
+			"Latitude" : 18.12,
+			"Longitude" : 21.26,
+			"House_No" : 221,
+			"Appartment_Name" : "Baker's Street",
+			"Street" : "Baker's Street",
+			"City" : "London",
+			"State" : "Bermingham",
+			"Landmark" : "S. H.",
+			"PIN" : 411043
+		},
+		"Owner Name" : "Mino laad",
+		"Email" : "minal.laad@gmail.com",
+		"Password" : "abcd",
+		"Mobile" : "8745123690",
+		"TypeOD" : ["COD","Pick_UP"],
+		"Min_Order" : 500,
+		"AverageRating" : 3.0,
+		"CurrentOrders" : [],
+		"PreviousOrders" : [],
+		"Categories" : ["Grocery", "FruitsnVegitables"],
+		"ProductsAvailable" : [{}]
+	},
+	{
+		"Shopname" : "Pune Icecream center",
+		"Address" : {
+			"Latitude" : 18.22,
+			"Longitude" : 21.31,
+			"House_No" : 281,
+			"Appartment_Name" : "Chinatown",
+			"Street" : "MG Road",
+			"City" : "Pune",
+			"State" : "Maharashtra",
+			"Landmark" : "Puppet Square",
+			"PIN" : 411043
+		},
+		"Owner Name" : "Jumma laad",
+		"Email" : "jumma@gmail.com",
+		"Password" : "abcd",
+		"Mobile" : "8745123690",
+		"TypeOD" : ["Pick_UP"],
+		"Min_Order" : 40,
+		"AverageRating" : 3.0,
+		"CurrentOrders" : [],
+		"PreviousOrders" : [],
+		"Categories" : ["Icecreams"],
+		"ProductsAvailable" : [{}]
+	},
+	{
+		"Shopname" : "Glucoke Personal Care",
+		"Address" : {
+			"Latitude" : 38.12,
+			"Longitude" : 51.26,
+			"House_No" : 21,
+			"Appartment_Name" : "Nova",
+			"Street" : "VK Road",
+			"City" : "Kathmandu",
+			"State" : "Nepal",
+			"Landmark" : "Lips of an Angel",
+			"PIN" : 411043
+		},
+		"Owner Name" : "Honey Okay",
+		"Email" : "honey@gmail.com",
+		"Password" : "abcd",
+		"Mobile" : "8745123690",
+		"TypeOD" : ["COD","Pick_UP"],
+		"Min_Order" : 1500,
+		"AverageRating" : 3.0,
+		"CurrentOrders" : [],
+		"PreviousOrders" : [],
+		"Categories" : ["Personal_Care"],
+		"ProductsAvailable" : [{}]
+	},
+	{
+		"Shopname" : "Tutu Singh da dhaba",
+		"Address" : {
+			"Latitude" : 25.78,
+			"Longitude" : 29.46,
+			"House_No" : 21,
+			"Appartment_Name" : "Tutu da dhaba",
+			"Street" : "Highway-26",
+			"City" : "Pummi chauk",
+			"State" : "Punjab",
+			"Landmark" : "National bank",
+			"PIN" : 411043
+		},
+		"Owner Name" : "Gogo Master",
+		"Email" : "gogo@gmail.com",
+		"Password" : "abcd",
+		"Mobile" : "8745123690",
+		"TypeOD" : ["COD","Pick_UP"],
+		"Min_Order" : 500,
+		"AverageRating" : 3.0,
+		"CurrentOrders" : [],
+		"PreviousOrders" : [],
+		"Categories" : ["Food"],
+		"ProductsAvailable" : [{}]
+	}
+]
